7d27c4b81ffa38103e92ee9cbb64e7ef3c32d7ed
stbam <55661516+stbam@users.noreply.github.com>
2025-05-19 12:53:16 -0400

    added movie css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,85 +1,52 @@
-import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Tabs, Tab } from "@mui/material";
 import '../App.css';
-import './Buttons'
+import './Buttons';
 import CustomButtons from "./Buttons";
-import { Avatar } from "@mui/material";
 import UserIcon from "./userIcon";
-import AutocompleteSearchBar from '../components/SearchBar';
+import AutocompleteSearchBar from './SearchBar';
 
-function Navbar({query,setQuery}) {
-   /* const [query, setQuery] = useState("");*/
-    const [results, setResults] = useState([]);
-    return (
-        <div>
-            {/* Top Navbar : web logo, search bar, sign in, register, user profile */}
-            <div className="top-bar">
-            <div className="divider top-line"></div>
-            <div className="divider bottom-line"></div>
-                    <nav>
-                        <Link to="/home" class="bingr">bingr</Link>
-                    </nav>
+function Navbar({ query, setQuery }) {
+  const location = useLocation();
 
-                    <AutocompleteSearchBar query={query} setQuery={setQuery} />
-                    <CustomButtons />
-                    <UserIcon />
-            </div>
+  const tabPaths = ["/home", "/books", "/movies", "/games", "/bugreport"];
+  const currentTab = tabPaths.includes(location.pathname) ? tabPaths.indexOf(location.pathname) : false;
 
-            {/* Bottom Navbar: Links: Home, Books, Movies, Games, Bug Reporting, Discord */}
-            <div className="under-bar">
-                <div className="left-links">
-                    <Link to="/home">Home</Link>
-                    <Link to="/books">Books</Link>
-                    <Link to="/movies">Movies</Link>
-                    <Link to="/games">Games</Link>
-                </div>
-                <div className="right-links">
-                    <Link to="bugreport">Bug Reporting</Link>
-                    <a href="https://discord.gg/nCGYy8HC">Discord</a>
-                   
-                </div>
-            </div>
+  return (
+    <div className="whole-bar">
+      {/* Top Navbar */}
+      <div className="top-bar">
+        <div className="divider top-line"></div>
+        <div className="divider bottom-line"></div>
+        <nav>
+          <Link to="/home" className="bingr">bingr</Link>
+        </nav>
+        <AutocompleteSearchBar query={query} setQuery={setQuery} />
+        <CustomButtons />
+        <UserIcon />
+      </div>
+
+      {/* Bottom Navbar Styled as Tabs */}
+      <div className="under-bar">
+        <div className="left-links">
+          <Tabs value={currentTab} textColor="inherit" TabIndicatorProps={{ style: { backgroundColor: "#000" } }}>
+            <Tab label="Home" component={Link} to="/home" className="tab-link" />
+            <Tab label="Books" component={Link} to="/books" className="tab-link" />
+            <Tab label="Movies" component={Link} to="/movies" className="tab-link" />
+            <Tab label="Games" component={Link} to="/games" className="tab-link" />
+            <Tab label="Discover" component={Link} to="/discovery" className="tab-link" />
+          </Tabs>
         </div>
-
-    );
-  }
-  
-  export default Navbar;
-
-  /*     <div>
-            <div
-                style={{
-                backgroundColor: "#222222",
-                height: "60px",
-                width: "100%",
-                position: "fixed",
-                top: "0",
-                left: "0",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                padding: "0 20px",
-                zIndex: "999",
-                }}
-            >
-                <nav>
-                    <a href="" class="bingr">bingr</a>
-                </nav>
-            </div>
-            <nav className="navbar">
-                <div className="left-links">
-                    <a href="">Home</a>
-                    <a href="">Books</a>
-                    <a href="">Movies</a>
-                    <a href="">Games</a>
-                </div>
-            </nav>
-                <nav className="navbar">
-                    <div className="right-links">
-                        <a href="">Bug Reporting</a>
-                        <a href="https://discord.gg/nCGYy8HC">Discord</a>
-                    </div>
-                </nav>
+        <div className="right-links">
+          <Tabs value={location.pathname === "/bugreport" ? 0 : false} textColor="inherit" TabIndicatorProps={{ style: { backgroundColor: "#000" } }}>
+            <Tab label="Bug Reporting" component={Link} to="/bugreport" className="tab-link" />
+            <Tab label="Discord" component="a" href="https://discord.gg/nCGYy8HC" target="_blank" className="tab-link" />
+          </Tabs>
         </div>
-  */
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
