2498815ae96cccf49354848651c256d3eaf471bc
stbam <55661516+stbam@users.noreply.github.com>
2025-05-19 12:04:33 -0400

    profile bug query imported in wrong line

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -12,7 +12,7 @@ function shuffleArray(array) {
   return [...array].sort(() => Math.random() - 0.5);
 }
 
-function Movies({query,setQuery}) {
+function Movies({query}) {
   const [moviesByGenre, setMoviesByGenre] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [selectedMovieId, setSelectedMovieId] = useState(null);
